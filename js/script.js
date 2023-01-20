30481d63fcf2c6f3d2ae626e043814c7c150469b
Salvatore Capano <capano_salvatore@libero.it>
2023-01-20 19:15:08 +0100

    Esercizio Completato

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const { createApp } = Vue;
+
+createApp({
+
+    data() {
+        return {
+            message: "Hello Vue!",
+            imgUrl: "https://ychef.files.bbci.co.uk/976x549/p07ryyyj.jpg"
+        }
+    }
+
+}).mount("#app");
